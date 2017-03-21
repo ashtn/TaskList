1fe223715162bb5924d0178f89fb1e10244820e8
Ashtn <ashtonehorton@gmail.com>
2017-03-21 16:53:14 -0700

    added controller action show for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/tasks/:id', to: 'tasks#show'
+  get "/tasks/:id", to: "tasks#show", as: 'task'
 end
