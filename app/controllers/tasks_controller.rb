6953a45a857b67315dec21038a6f142b894ab388
Ashtn <ashtonehorton@gmail.com>
2017-03-23 23:20:43 -0700

    added edit link to show page, created edit route, edit action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "/tasks", to: "tasks#create"
   get '/tasks', to: 'tasks#index'
   get "/tasks/:id", to: "tasks#show", as: 'task'
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
 end
 
 
