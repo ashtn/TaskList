f91c779f7c32de8be9ab01035f1031907864814e
Ashtn <ashtonehorton@gmail.com>
2017-03-24 14:24:19 -0700

    added partial form file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   root "tasks#index"
+
+  #create
   get "/tasks/new", to: "tasks#new", as: "new_task"
   post "/tasks", to: "tasks#create"
+
+  #read
   get '/tasks', to: 'tasks#index'
   get "/tasks/:id", to: "tasks#show", as: 'task'
+
+  #update
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   put "tasks/:id", to: "tasks#update"
+  patch "tasks/:id", to: "tasks#complete", as: "complete_task"
+
+  #destroy
+  delete "tasks/:id", to: "task#destroy", as: "delete_task"
 end
 
 
